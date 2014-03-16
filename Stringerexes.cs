2afd4df8d04ca5e0cfa5868e339c4aa50a6ad48d
Timwi <github@timwi.de>
2014-03-16 18:06:33 +0100

    More predefined Stringerexes (Letter, LetterOrDigit, ConnectorPunctuation, Identifier, IdentifierNoPunctuation, JavaScriptIdentifier)

--- Stringerexes.cs
+++ Stringerexes.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 
 namespace RT.Generexes
 {
@@ -29,5 +27,20 @@ namespace RT.Generexes
                 .Do(m => m.Length > 0))
             .Then(Stringerex.Ors("e", "E").Then(Stringerex.Ors("+", "-", "")).Then(Digit0to9.RepeatGreedy(1)).OptionalGreedy())
             .Process(m => Convert.ToDouble(m.Match));
+
+        /// <summary>Parses a single letter character (Unicode category L).</summary>
+        public static readonly Stringerex Letter = new Stringerex(char.IsLetter);
+        /// <summary>Parses a single character that is a letter or digit (Unicode category L or Nd).</summary>
+        public static readonly Stringerex LetterOrDigit = new Stringerex(char.IsLetterOrDigit);
+        /// <summary>Parses a single character that belongs to Unicode category Pc. This includes the underscore.</summary>
+        public static readonly Stringerex ConnectorPunctuation = new Stringerex(c => char.GetUnicodeCategory(c) == UnicodeCategory.ConnectorPunctuation);
+        /// <summary>
+        ///     Parses an identifier according to the rules of most programming languages (allowing the underscore but no
+        ///     dollar sign).</summary>
+        public static readonly Stringerex Identifier = Letter.Or(ConnectorPunctuation).Then(LetterOrDigit.Or(ConnectorPunctuation).RepeatGreedy());
+        /// <summary>Parses an identifier without allowing the underscore.</summary>
+        public static readonly Stringerex IdentifierNoPunctuation = Letter.Then(LetterOrDigit.RepeatGreedy());
+        /// <summary>Parses a JavaScript identifier, which allows the dollar ($) character.</summary>
+        public static readonly Stringerex JavaScriptIdentifier = Letter.Or(ConnectorPunctuation).Or('$').Then(LetterOrDigit.Or(ConnectorPunctuation).Or('$').RepeatGreedy());
     }
 }
